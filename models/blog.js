dc05b0be11a254b1461309c7c4895384fe542690
Esteve2604 <estebanasperuiz@gmail.com>
2023-02-18 13:50:09 +0100

    Parte 4-2 casi terminada

--- models/blog.js
+++ models/blog.js
@@ -5,4 +5,10 @@ const blogSchema = new mongoose.Schema({
     url: String,
     likes: Number
   })
+blogSchema.set('toJSON', {
+  transform : (document, retunedObject) => {
+    retunedObject.id=retunedObject._id.toString()
+    delete retunedObject._id
+  }
+})
 module.exports= mongoose.model('blog', blogSchema)
--- tests/blog.api.test.js
+++ tests/blog.api.test.js
@@ -28,12 +28,53 @@ beforeEach(async () => {
     BlogObject = new Blog(initialBlogs[1])
     await BlogObject.save()
 })
-test('kc', async () => {
-    const response = await api.get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-    expect(response.body).toHaveLength(initialBlogs.length)
-}, 100000)
+describe('blogTest', () => {
+    test('response and length', async () => {
+        const response = await api.get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        expect(response.body).toHaveLength(initialBlogs.length)
+
+    }, 1000000) //aumenta el timeOut
+    test('id is defined', async () => {
+        const response = await api.get('/api/blogs')
+        response.body.forEach(blog => expect(blog.id).toBeDefined())
+    })
+    test('post works and likes don\'t change', async () => {
+        const objectSent = {
+            "title": "buenas",
+            "author": "tardes",
+            "url": "jesucristo",
+            "likes": 14
+        }
+        const response = await api.post('/api/blogs').send(objectSent).expect(201)
+        expect(response.body.likes).toBe(objectSent.likes)
+        const verify = await api.get('/api/blogs')
+        expect(verify.body).toHaveLength(initialBlogs.length + 1)
+    })
+    test('post with missing likes', async () => {
+        const objectSent = {
+            "title": "dia",
+            "author": "quiensabra",
+            "url": "nose",
+        }
+        const response = await api.post('/api/blogs').send(objectSent).expect(201)
+        expect(response.body.likes).toBe(0)
+    })
+    test('post with missing url or title returns 400 status', async () => {
+        const objectSent1 = {
+            "author": "quiensabra",
+            "url": "nose",
+        }
+        const objectSent2 = {
+            "title": "dia",
+            "author": "quiensabra",
+        }
+        await api.post('/api/blogs').send(objectSent1).expect(400)
+        await api.post('/api/blogs').send(objectSent2).expect(400)
+    })
+})
+
 afterAll(async () => {
     await mongoose.connection.close()
 })
