d59a659136cf637c053fcc1e43590e4c10d5a8e7
Esteve2604 <estebanasperuiz@gmail.com>
2023-02-20 21:20:13 +0100

    parte 4.22 correcta

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -14,6 +16,14 @@ const tokenExtractor = (request, response, next) => {
   }
   next()
 }
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    request.user = await User.findById(decodedToken.id)
+    next()
+}
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -27,6 +37,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'ReferenceError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -35,6 +47,7 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   requestLogger,
   tokenExtractor,
+  userExtractor,
   unknownEndpoint,
   errorHandler
 }
